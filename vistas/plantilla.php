47efea35316feec75ca2914df45e4a1a7862ae6c
Ramiro Diaz <milinchi_@hotmail.com>
2019-04-15 20:38:25 -0500

    modificacion plantilla
    se modificio la plantilla

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -13,44 +13,39 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
  
      <!-- Site Metas -->
-    <title></title>  
+    <title>Greatseo - Bootstrap 4 Responsive HTML5 Template</title>  
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Site Icons -->
-    <link rel="shortcut icon" href="vistas/images/logo-cucas-kain.png" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="vistas/images/apple-touch-icon.png">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="vistas/css/bootstrap.min.css">
     <!-- Site CSS -->
-    <link rel="stylesheet" href="vistas/style.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="vistas/css/versions.css">
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="vistas/css/responsive.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="vistas/css/custom.css">
-    <link rel="stylesheet" href="vistas/themes/orange/pace-theme-minimal.css">
-    <!-- Modernizer for Portfolio -->
-    <script src="vistas/js/modernizer.js"></script>
-    <script src="vistas/js/modernizer.js"></script>
-    <script src="vistas/js/all.js"></script>
-    <!-- ALL PLUGINS -->
-    <script src="vistas/js/custom.js"></script>
-    <script src="vistas/js/portfolio.js"></script>
-    <script src="vistas/js/hoverdir.js"></script>   
-    <script src="vistas/js/ir-arriba.js"></script>  
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
-
-    <script src="vistas/js/pace.min.js"></script>   
-
+	
+	<script src="vistas/js/modernizr.custom.79639.js"></script>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <script src="vistas/js/all.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="vistas/js/tippy.all.min.js"></script>
+    <script src="vistas/js/custom.js"></script>
+	
+	<script src="vistas/js/jquery.ba-cond.min.js"></script>
+	<script src="vistas/js/jquery.slitslider.js"></script>
 </head>
-
 <!--=====================================
 CUERPO DOCUMENTO
 ======================================-->
