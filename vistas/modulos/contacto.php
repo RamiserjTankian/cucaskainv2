730ce65bf6bd1912088f17d59f8d6696c5eadb27
Ramiro Diaz <milinchi_@hotmail.com>
2019-04-15 23:47:41 -0500

    Cambios en modulos y estilos
    Cambios generales en modulos y estilos.

--- vistas/modulos/contacto.php
+++ vistas/modulos/contacto.php
@@ -1,24 +1,4 @@
-<div class="all-page-title" style="background-image:url(images/header.jpg);">
-		<!--Page -->
-        <div class="page-info">
-            <div class="container">
-            	<div class="inner-container">
-                    <ul class="bread-crumb">
-                        <li><a href="inicio">Inicio</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
-                        <li><span>Contacto</span></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!--End Page-->
-        <div class="container text-center">
-            <h1>Contacto</h1>
-			<p>Comunicate con nosotros para obtener más información y separar tu cita.</p>
-        </div>
-    </div><!-- end section -->
-
-    <div id="support" class="section wb">
+<div id="support" class="section wb">
         <div class="container">
             <div class="section-title text-center">
                 <h3>Get In Touch</h3>
@@ -26,38 +6,34 @@
             </div><!-- end title -->
 
             <div class="row">
-                <div class="col-md-6">
-                    <div class="contact_form">
+                <div class="col-md-8 offset-md-2">
+                    <div class="contact_form" style="background-color :#c1bdbd;">
                         <div id="message"></div>
-                        <form id="contactform" class="row" action="contact.php" name="contactform" method="post">
-                            <fieldset class="row row-fluid">
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <form id="contactform" action="contact.php" name="contactform" method="post">
+                            <fieldset class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-6">
                                     <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name">
                                 </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                <div class="col-lg-12 col-md-12 col-sm-6">
                                     <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name">
                                 </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                <div class="col-lg-12 col-md-12 col-sm-6">
                                     <input type="email" name="email" id="email" class="form-control" placeholder="Your Email">
                                 </div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                    <input type="text" name="phone" id="phone" class="form-control" placeholder="Your Website">
-                                </div>                                
+                                <div class="col-lg-12 col-md-12 col-sm-6">
+                                    <input type="text" name="phone" id="phone" class="form-control" placeholder="Your Phone">
+                                </div>
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                     <textarea class="form-control" name="comments" id="comments" rows="6" placeholder="Give us more details.."></textarea>
                                 </div>
-                                <div class="text-center img-st">
-                                    <button type="submit" value="SEND" id="submit" class="bttn-new-a">Get a Quote</button>
+                                <div class="text-center cont-btn">
+                                    <button type="submit" value="SEND" id="submit" class="btn11"><span>Submit</span></button>
                                 </div>
                             </fieldset>
                         </form>
                     </div>
                 </div><!-- end col -->
-				<div class="col-md-6">
-					<div class="map-box">
-						<div id="custom-places" class="small-map"></div>
-					</div>
-				</div><!-- end col -->
+            </div><!-- end row -->
             </div><!-- end row -->
         </div><!-- end container -->
     </div><!-- end section -->
@@ -78,5 +54,3 @@
             </div><!-- end row -->
         </div><!-- end container -->
     </div><!-- end section -->
-
-	
--- vistas/modulos/header.php
+++ vistas/modulos/header.php
@@ -25,18 +25,8 @@
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item active"><a class="nav-link" href="inicio">Inicio</a></li>
 						<li class="nav-item"><a class="nav-link" href="nosotros">Nosotros</a></li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Servicios</a>
-							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="services.html">Plaga A</a>
-								<a class="dropdown-item" href="services.html">Plaga B</a>
-								<a class="dropdown-item" href="services.html">Plaga C</a>
-								<a class="dropdown-item" href="services.html">Plaga D</a>
-								<a class="dropdown-item" href="services.html">Plaga E</a>
-								<a class="dropdown-item" href="services.html">Plaga F</a>
-							</div>
-						</li>
-						<li class="nav-item"><a class="nav-link" href="precios">Precios</a></li>
+						<li class="nav-item"><a class="nav-link" href="servicios">Servicios</a></li>
+						<li class="nav-item"><a class="nav-link" href="plagas">Plagas</a></li>
 						<li class="nav-item"><a class="nav-link" href="contacto">Contacto</a></li>
 					</ul>
 				</div>
