847bc058116016ee11268ad3448942aa174217d5
Estebanyzq <estebanyrazoqui@gmail.com>
2019-04-15 20:25:25 -0500

    Estructura de Pagina Web

---
+++ vista/modulos/404.php
@@ -0,0 +1,25 @@
+<div class="banner-area banner-bg-1">
+<div class="container">
+<div class="row">
+    <div class="col-sm-6 col-md-6">
+        <img class=" img-fluid" src="vistas/images/logo-cucas-kain.png" >
+         </div>
+            <div class="col-md-6">
+            <div class="shadow p-3 mb-5 bg-white rounded">
+         
+                <div class="section-title text-center">
+
+                    <h1>ERROR 404</h1>
+                                       
+                  <p>Esta pagina no existe</p>
+            <br>
+            <br>
+                    <button type="button" class="btn btn-warning btn-lg">Volver al inicio</button>
+                </div><!-- end messagebox -->
+            </div>  
+            </div>
+            </div>
+    </div>
+</div>
+</div>
+</div>
